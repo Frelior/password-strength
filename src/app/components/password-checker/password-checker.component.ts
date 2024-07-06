554ebdceb2152864ebb0a10c1346127239ffd86e
Michael Rudenko <annoyedmichael@gmail.com>
2024-07-06 14:27:47 +0300

    Added error message

--- src/app/components/password-checker/password-checker.component.ts
+++ src/app/components/password-checker/password-checker.component.ts
@@ -14,6 +14,18 @@ export class PasswordCheckerComponent implements OnInit {
     password: new FormControl(''),
   });
   strengthClasses: string[] = ['gray', 'gray', 'gray'];
+  errorMessages: {
+    short: string;
+    easy: string;
+    medium: string;
+    strong: string;
+  } = {
+    short: 'Password must be at least 8 characters',
+    easy: 'Password is too easy',
+    medium: 'Password is medium',
+    strong: 'Password is strong',
+  };
+  currentError: string | null = null;
 
   ngOnInit(): void {
     this.passwordForm
@@ -26,27 +38,32 @@ export class PasswordCheckerComponent implements OnInit {
   public updatePasswordIndicators(password: string): void {
     if (!password) {
       this.strengthClasses = ['gray', 'gray', 'gray'];
+      this.currentError = null;
       return;
     }
     if (password.length < 8) {
       this.strengthClasses = ['red', 'red', 'red'];
+      this.currentError = this.errorMessages.short;
       return;
     }
 
     const hasLetters = /[a-zA-Zа-яА-ЯёЁіІїЇґҐ]/.test(password);
     const hasDigits = /\d/.test(password);
-    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const hasSymbols = /[!@#$%^&*(),.?":{}|<>№;]/.test(password);
 
     if (hasLetters && hasDigits && hasSymbols) {
       this.strengthClasses = ['green', 'green', 'green'];
+      this.currentError = this.errorMessages.strong;
     } else if (
       (hasLetters && hasDigits) ||
       (hasLetters && hasSymbols) ||
       (hasDigits && hasSymbols)
     ) {
       this.strengthClasses = ['yellow', 'yellow', 'gray'];
+      this.currentError = this.errorMessages.medium;
     } else {
       this.strengthClasses = ['red', 'gray', 'gray'];
+      this.currentError = this.errorMessages.easy;
     }
   }
 }
